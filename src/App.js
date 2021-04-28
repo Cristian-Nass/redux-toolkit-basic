b0742ef1fe5fdd8db12a6267ee20f9e586075aa4
Cristian <cristian.tirdad@gmail.com>
2021-04-29 00:38:12 +0200

    add by custom number

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import {useDispatch, useSelector} from "react-redux";
-import {increment, decrement} from "./redux/CounterSlice";
+import {increment, decrement, test} from "./redux/CounterSlice";
 
 
 
@@ -12,8 +12,9 @@ function App() {
   return (
     <div className="App">
       <h1> The count is: {count}</h1>
-      <button onClick={() => dispatch(increment())}> increment </button>
-      <button onClick={() => dispatch(decrement())}> decrement </button>
+      <button onClick={() => dispatch(increment())}> Increment </button>
+      <button onClick={() => dispatch(decrement())}> Decrement </button>
+      <button onClick={() => dispatch(test(5))}> Add by 5 </button>
     </div>
   );
 }
--- src/redux/CounterSlice.js
+++ src/redux/CounterSlice.js
@@ -4,11 +4,12 @@ const counterSlice = createSlice({
   name: "counter",
   initialState: 0,
   reducers: {
-    increment: state =>  state += 1 ,
-    decrement: state =>  state -= 1 
+    increment: state =>  state += 1,
+    decrement: state =>  state -= 1,
+    test: (state, action) => state += action.payload,
   }
 });
 
-export const {increment, decrement} = counterSlice.actions;
+export const {increment, decrement, test} = counterSlice.actions;
 
 export default counterSlice.reducer;
